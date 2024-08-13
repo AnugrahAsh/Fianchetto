86c290117e263641f37a631ef86d69877c7775aa
Anugrah Sharma <127242736+AnugrahAsh@users.noreply.github.com>
2024-08-13 20:42:02 +0530

    addition of new pages

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const socket = require('socket.io');
 const http = require('http');
 const { Chess } = require('chess.js');
 const path = require('path');
+const mongoose = require('mongoose');
+const User = require('./models/user')
+
+const bodyParser = require('body-parser');
+
+
+mongoose.connect('mongodb://localhost:27017/Fianchetto');
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 const app = express();
 const server = http.createServer(app);
@@ -14,9 +26,13 @@ let currentPlayer = "w";
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-app.get('/', (req, res) => {
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/gameroom', (req, res) => {
     res.render('index', { title: "Chess Game" });
 });
+
 
 io.on("connection", function (uniqueSocket) {
     console.log("Connected");
@@ -31,9 +47,9 @@ io.on("connection", function (uniqueSocket) {
         uniqueSocket.emit("spectatorRole");
     }
 
-    uniqueSocket.on('chatMessage',({message})=>{
-        io.emit('message', message);
-    })
+    // uniqueSocket.on('chatMessage',({message})=>{
+    //     io.emit('message', message);
+    // })
 
     uniqueSocket.on("disconnect", () => {
         if (uniqueSocket.id === players.white) {
@@ -64,6 +80,28 @@ io.on("connection", function (uniqueSocket) {
         }
     });
 });
+app.get('/', (req, res) => {
+    res.render('home', { title: "Fianchetto" });
+});
+
+app.get('/login', (req, res)=>{
+    res.render('login');
+})
+
+app.get('/register', (req, res)=>{
+    res.render('register');
+})
+
+app.post('/games', async(req, res)=>{
+    const { email, username, password } = req.body.user;
+    const user = new User({ email, username, password });
+    await user.save();
+    res.redirect('/');
+})
+
+app.get('*', (req, res)=>{
+    res.render('pageNotFound')
+})
 
 server.listen(3000, () => {
     console.log("Listening on port 3000");
